6ea0afb12271bfa771ac1ac37f9ce7e5e7d8acbc
valeriamore <69865723+valeriamore@users.noreply.github.com>
2021-03-01 09:41:29 -0500

    Update ecuacion.c

--- ecuacion.c
+++ ecuacion.c
@@ -1,47 +1,44 @@
 //* ECUACION RECTA*//
 
-#include <stdio.h>
-#include <stdlib.h>
-struct punto{
-        float x; float y;
-};
-int main() {
-        float pen;
-        struct punto p1,p2;
-        void pedir_puntos1 (struct punto*,struct punto*);
-        void pendiente(float*,struct punto,struct punto);
-        void ecuacion(float*,struct punto,struct punto);
-        pedir_puntos1(&p1,&p2);
-        pendiente(&pen,p1,p2);
-        ecuacion(&pen,p1,p2);
+#include <stdio.h> //Librerias//
+#include <stdlib.h> //Librerias//
+
+struct punto{ float x; float y; }; //se inicializa la estructua con los valores x,y//
+void pedir_puntos1 (struct punto*,struct punto*); //se llama la funcion de pedir puntos//
+void pendiente(float*,struct punto,struct punto); //se llama la funcion pendiente//
+void ecuacion(float*,struct punto,struct punto);// se llama a la funcion para imprimir la ecuacion//
+
+
+int main() {                      // se crea la funcion main donde se va a llamar a las funciones//
+        float pen;                //float para valor pendiente
+        struct punto p1,p2;       //struct con punto p1,p2
+        pedir_puntos1(&p1,&p2);   //apuntadores
+        pendiente(&pen,p1,p2);    //apuntadores
+        ecuacion(&pen,p1,p2);     //apuntadores
 }
-void pedir_puntos1(struct punto *p1,struct punto *p2 ){
-        printf("\ningrese datos de x1\n");
+void pedir_puntos1(struct punto *p1,struct punto *p2 ){   //funcion para pedir puntos
+        printf("\ningrese datos de x1\n");     //se pide el ingreso de datos x1
         scanf("\n%f",&p1->x );
-        printf("\ningrese datos de y1\n");
+        printf("\ningrese datos de y1\n");    //se pide el ingreso de datos y1
         scanf("\n%f",&p1->y);
-        printf("\ningrese datos de x2\n");
+        printf("\ningrese datos de x2\n");   //se pide el ingreso de datos x2
         scanf("\n%f",&p2->x );
-        printf("\ningrese datos de y2\n");
+        printf("\ningrese datos de y2\n");   //se pide el ingreso de datos y2
         scanf("\n%f",&p2->y);
-        // printf("\n%f %f\n",p1->x,p1->y);
-        // printf("\n%f %f\n",p2->x,p2->y);
+
 }
-void pendiente(float * pen,struct punto p1,struct punto p2 ){
+void pendiente(float * pen,struct punto p1,struct punto p2 ){ // Funcion para hallar la pendiente
 
-	float  d=*pen;
+	float  d=*pen;           // calculo pendiente
 
-	d=((p2.y)-(p1.y))/((p2.x)-(p1.x));
-     
-
-	*pen=d;
+	*pen=d; 		//apuntador pendiente
 }
-void ecuacion(float* pen,struct punto p1,struct punto p2){
+void ecuacion(float* pen,struct punto p1,struct punto p2){  //funcion para hallar el valor b de la ecuacion e imprimir
 	float d=*pen;
         float b;
-        b= -(d*p1.x) + p1.y;
+        b= -(d*p1.x) + p1.y;     // hallar el punto de corte de la ecuacion
         printf("\n La ecuacion de la recta es:\n");
         printf("\n Y= %.3fx %.3f\n",d,b);
   
-	*pen=d;
+	*pen=d;    //apuntador pendiente
 }
